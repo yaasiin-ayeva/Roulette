740fee9f3fd8b7f03dfabb749a95b364aa46c471
Yaasiin AYEVA <ayevayaasiin@gmail.com>
2023-08-11 04:24:38 +0000

    SCSS Update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,28 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(
+    private toastController: ToastController,
+  ) { }
+  ngOnInit(): void {
+    this.presentSuccessToast('bottom', 'Home Page');
+  }
 
   grid: string[][] = new Array(6).fill([]).map(() => new Array(4).fill(''));
-  buttons: string[] = ['Button 1', 'Button 2', 'Button 3', 'Button 4'];
+  buttons: string[] = ['+', '-', 'Statistics', 'Table'];
 
   validateValue(rowIndex: number, colIndex: number) {
     const inputValue = Number(this.grid[rowIndex][colIndex]);
     if (inputValue && inputValue <= 0 && inputValue > 36) {
-      // Reset the input if the value is not allowed
       this.grid[rowIndex][colIndex] = '';
     }
   }
 
   handleButtonClick(button: string) {
-    // Handle button click logic here
-    console.log('Button clicked:', button);
+
   }
 
+  async presentSuccessToast(position: 'top' | 'middle' | 'bottom', message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 1500,
+      position: position,
+      cssClass: 'toast',
+      color: 'tertiary',
+      mode: 'md'
+    });
+
+    await toast.present();
+  }
 }
