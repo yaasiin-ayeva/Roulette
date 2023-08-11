51529b0ec41494fd27e9d4c97e8bfbcfe97030db
Yaasiin AYEVA <ayevayaasiin@gmail.com>
2023-08-11 04:07:46 +0000

    Apply SCSS Style

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,20 @@ export class HomePage {
 
   constructor() {}
 
+  grid: string[][] = new Array(6).fill([]).map(() => new Array(4).fill(''));
+  buttons: string[] = ['Button 1', 'Button 2', 'Button 3', 'Button 4'];
+
+  validateValue(rowIndex: number, colIndex: number) {
+    const inputValue = Number(this.grid[rowIndex][colIndex]);
+    if (inputValue && inputValue <= 0 && inputValue > 36) {
+      // Reset the input if the value is not allowed
+      this.grid[rowIndex][colIndex] = '';
+    }
+  }
+
+  handleButtonClick(button: string) {
+    // Handle button click logic here
+    console.log('Button clicked:', button);
+  }
+
 }
