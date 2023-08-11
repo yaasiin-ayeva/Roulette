cb71fc6b1e0ce4e6a2cb24f5dd8c889577127fd3
Yaasiin AYEVA <ayevayaasiin@gmail.com>
2023-08-11 06:22:44 +0000

    Simple Array with Grid

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ToastController } from '@ionic/angular';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,25 +8,72 @@ import { ToastController } from '@ionic/angular';
 })
 export class HomePage implements OnInit {
 
+  values: string[];
+
   constructor(
     private toastController: ToastController,
-  ) { }
+    private alertController: AlertController
+  ) {
+    this.values = new Array(24).fill('');
+  }
+
   ngOnInit(): void {
     this.presentSuccessToast('bottom', 'Home Page');
   }
 
-  grid: string[][] = new Array(6).fill([]).map(() => new Array(4).fill(''));
-  buttons: string[] = ['+', '-', 'Statistics', 'Table'];
+  buttons: string[] = ['+', '-', 'Statistics', 'Wipe'];
 
-  validateValue(rowIndex: number, colIndex: number) {
-    const inputValue = Number(this.grid[rowIndex][colIndex]);
-    if (inputValue && inputValue <= 0 && inputValue > 36) {
-      this.grid[rowIndex][colIndex] = '';
+  validateValue(colIndex: number) {
+
+    const inputValue = Number(this.values[colIndex]);
+
+    if (!isNaN(inputValue) && inputValue >= 0 && inputValue <= 36) {
+      this.values[colIndex] = inputValue.toString();
     }
   }
 
   handleButtonClick(button: string) {
+    if (button === 'Wipe') {
+      console.log('Wipe');
+      this.wipeGrid();
+    }
+  }
 
+  getRegexPattern(): string {
+    return '^(?:[0-9]|[1-2][0-9]|3[0-6])$';
+  }
+
+  onChange(event: any, index: number) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    const pattern = /^(?:[0-9]|[1-2][0-9]|3[0-6])$/;
+
+    if (!pattern.test(newValue)) {
+      input.value = this.values[index];
+    } else {
+      this.values[index] = newValue;
+    }
+  }
+
+  private wipeGrid() {
+    this.alertController.create({
+      header: 'Wipe Grid',
+      message: 'Are you sure you want to wipe the grid?',
+      mode: 'ios',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel'
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            this.values = new Array(24).fill('');
+            this.presentSuccessToast('bottom', 'Grid Cleared');
+          }
+        }
+      ]
+    }).then(alert => alert.present());
   }
 
   async presentSuccessToast(position: 'top' | 'middle' | 'bottom', message: string) {
@@ -35,8 +82,8 @@ export class HomePage implements OnInit {
       duration: 1500,
       position: position,
       cssClass: 'toast',
-      color: 'tertiary',
-      mode: 'md'
+      color: 'medium',
+      mode: 'ios',
     });
 
     await toast.present();
