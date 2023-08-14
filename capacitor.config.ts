7ea7aef55aac69cb292e03fa85dd32fc30050e75
Yaasiin AYEVA <ayevayaasiin@gmail.com>
2023-08-14 19:13:48 +0000

    Data Fetching OK

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,13 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.ayevayaasiin.roulette',
   appName: 'Roulette',
   webDir: 'www',
   server: {
-    androidScheme: 'https'
+    androidScheme: 'http',
+    url: 'http://197.253.123.12:9009/api/v1/roulette',
+    iosScheme: 'http',
   }
 };
 
